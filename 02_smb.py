8de7ae1e481dc79adc27116507008f51b2a08bf7
Branddoon <jairgrande.22@gmail.com>
2025-06-13 21:36:43 -0400

    Fix: SMB scanning file logic

--- 02_smb.py
+++ 02_smb.py
@@ -6,7 +6,7 @@ class SMBScanner:
         self.ip_target = ip_target
     
     def scann_smb(self):
-        device_dict = {}
+        resource_dict = {}
         try:
             conn = SMBConnection(
                 "",
@@ -18,14 +18,15 @@ class SMBScanner:
             )
             if conn.connect(self.ip_target, 445):
                 print(f"Connection has been stablished at ip:{self.ip_target}")
-                for device in conn.listShares():
-                    files = conn.listPath(device, '/')
-                    device_dict[device.name] = [file.filename for file in files]          
+                for resource in conn.listShares():
+                    if not resource.isSpecial and resource.name not in ['NETLOGON', 'SYSVOL']:
+                        files = conn.listPath(resource.name, '/')
+                        resource_dict[resource.name] = [file.filename for file in files]         
             else:
                 print('Connection was not stablished.')
         except Exception as e:
             print(f"Error during SMB scanning execution.{e}")
-        return device_dict
+        return resource_dict
 
 if __name__ == '__main__':
     sbm_scanner = SMBScanner("192.168.238.132")
